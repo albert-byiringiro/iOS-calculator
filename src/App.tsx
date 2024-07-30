6cbd77f4aa3f38f91e035b49008e11bc206c19dd
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-30 12:27:34 +0200

    Initial calculator design retaches

--- src/App.tsx
+++ src/App.tsx
@@ -7,63 +7,81 @@ export default function App() {
         </span>
       </div>
       <form className="grid grid-cols-4">
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">AC</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">+/-</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">%</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center bg-orange-400 text-white">
-          <button className="">/</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">7</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">8</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">9</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center bg-orange-400 text-white">
-          <button className="">X</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">4</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">5</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">6</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center bg-orange-400 text-white">
-          <button className=""> - </button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">1</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">2</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">3</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center bg-orange-400 text-white">
-          <button className="">+</button>
-        </label>{" "}
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center col-span-2">
-          <button className="">0</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center">
-          <button className="">.</button>
-        </label>
-        <label className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-bold flex justify-center content-center bg-orange-400 text-white">
-          <button className=""> = </button>
-        </label>
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          AC
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          +/-
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          %
+        </button>
+
+        <button className="px-1 py-8  border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center bg-orange-500 text-white">
+          /
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          7
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          8
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          9
+        </button>
+
+        <button className="px-1 py-8  border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center bg-orange-500 text-white">
+          X
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          4
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          5
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          6
+        </button>
+
+        <button className="px-1 py-8 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center bg-orange-500 text-white">
+          -
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          1
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          2
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          3
+        </button>
+
+        <button className="px-1 py-8 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center bg-orange-500 text-white">
+          +
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center col-span-2">
+          0
+        </button>
+
+        <button className="px-1 py-8 bg-gray-100 border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center">
+          .
+        </button>
+
+        <button className="px-1 py-8  border-2 border-gray-600 text-3xl font-semibold flex justify-center content-center bg-orange-500 text-white">
+          =
+        </button>
       </form>
     </main>
   );
