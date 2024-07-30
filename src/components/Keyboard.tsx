f509b1c4a432cf98aa306c9a5f905282e9801e71
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-30 20:10:52 +0200

    Calculator: add interface for keyboard props

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,3 +1,14 @@
+interface KeyboardProps {
+  display: string;
+  onNumberInput: (num: string) => void;
+  onPeratorInput: (op: string) => void;
+  onEquals: () => void;
+  onClear: () => void;
+  onDecimal: () => void;
+  onPercentage: () => void;
+  onToggleSign: () => void;
+}
+
 export default function Keyboard() {
   return (
     <main>
