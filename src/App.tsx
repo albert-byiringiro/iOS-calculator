fa4ba553600195b919623d5ac5fbf9e4deffa6c4
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-30 19:21:54 +0200

    Calculator: add supposable states

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Keyboard from "./components/Keyboard";
 
-export default function App(){
+export default function App() {
   // State variables
-  const [display, setDisplay]
+  const [display, setDisplay] = useState("0");
+  const [prevValue, setPrevValue] = useState<number | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
+  const [waitingForOperand, setWaitingForOperand] = useState(false);
 }
