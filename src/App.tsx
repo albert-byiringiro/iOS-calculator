b2a03602c3d1c8fad77a58b04d99a9d7973e1fce
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-07-30 19:19:04 +0200

    Calculator: reset everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import React, {useState} from "react";
+
 import Keyboard from "./components/Keyboard";
 
-export default function App() {
-  const [selectedNumber, setSelectedNumber] = useState("");
-  const [symbol, setSymbol] = useState("");
-
-  const selectNumber = (e: ChangeEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setSelectedNumber(e.target.value);
-    console.log(e.target.value);
-  };
-
-  const selectSymbol = (e: ChangeEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setSelectedNumber(e.target.value);
-    console.log(e.target.value);
-  };
-
-  return <Keyboard selectNumber={selectNumber} selectSymbol={selectSymbol} />;
+export default function App(){
+  // State variables
+  const [display, setDisplay]
 }
